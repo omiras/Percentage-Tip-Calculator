880e5a4cabe2ffbc861921aa60cb8de8f8b50c65
omiras <oscar.miras@gmail.com>
2023-10-23 12:20:15 +0200

    solution fixed

--- solution/app.js
+++ solution/app.js
@@ -1,26 +1,21 @@
-let billAmount = document.getElementById('bill-amount');
-let percentTip = document.getElementById('percentage-tip');
-let tipAmount = document.getElementById('tip-amount');
-let total = document.getElementById('total');
-let calculate = document.getElementById('calculate');
+let percentTip = document.getElementById("percentage-tip");
+let tipAmount = document.getElementById("tip-amount");
+let total = document.getElementById("total");
+let calculate = document.getElementById("calculate");
 
-calculate.addEventListener('click', function(){
-    billAmount = Number(billAmount.value);
-    percentTip = Number(percentTip.value);
+calculate.addEventListener("click", function () {
+  billAmount = Number(document.getElementById("bill-amount").value);
+  percentTip = Number(document.getElementById("percentage-tip").value);
 
-    if (isNaN(billAmount) || billAmount <= 0 || billAmount === null){
-        alert('Please enter a valid Bill Amount')
-    }
-    else if (isNaN(percentTip) || percentTip <= 0 || percentTip === null){
-        alert('Please enter a valid Tip Percentage')
-    }
-    else {
-        let calculatedTip =  percentTip/100*billAmount;
-        tipAmount.value = '$' + calculatedTip;
+  if (isNaN(billAmount) || billAmount <= 0 || billAmount === null) {
+    alert("Please enter a valid Bill Amount");
+  } else if (isNaN(percentTip) || percentTip <= 0 || percentTip === null) {
+    alert("Please enter a valid Tip Percentage");
+  } else {
+    let calculatedTip = (percentTip / 100) * billAmount;
+    tipAmount.value = "$" + calculatedTip;
 
-        let calculatedTotal = billAmount + calculatedTip;
-        total.value = '$' + calculatedTotal;
-    }
-    
-
-})
+    let calculatedTotal = billAmount + calculatedTip;
+    total.value = "$" + calculatedTotal;
+  }
+});
